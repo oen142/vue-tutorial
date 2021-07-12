09f8a90d9c6b5b9c5a924b3e7884d8f2ff4de586
oen142 <oen142@naver.com>
2021-07-12 23:44:42 +0900

    완벽가이드 코드 init

---
+++ vue-news/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <ToolBar></ToolBar>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import ToolBar from "./components/ToolBar.vue";
+
+export default {
+  name: 'App',
+  components: {
+    ToolBar
+  },
+  methods: {}
+}
+</script>
+
+<style>
+body {
+  padding: 0;
+  margin: 0;
+}
+</style>
